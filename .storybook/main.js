640ad66f58b1fbdfef7f351c024d7dd0591e12f5
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-02-06 19:56:44 -0300

    Add Chakra UI

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,8 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/preset-create-react-app'
+		'@storybook/preset-create-react-app',
+		'@chakra-ui/storybook-addon'
 	],
 	framework: '@storybook/react',
 	core: {
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,8 @@
+import { themes } from '@storybook/theming'
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
+	docs: { theme: themes.light },
 	controls: {
 		matchers: {
 			color: /(background|color)$/i,
