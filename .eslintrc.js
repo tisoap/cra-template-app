78d32651fae639bd7717c21aa32e5c2ab4d7c2f2
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-02-06 16:55:38 -0300

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const tsConfigPath = path.resolve(__dirname, 'tsconfig.json')
+
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		ecmaFeatures: {
+			impliedStrict: true,
+			jsx: true
+		},
+		tsconfigRootDir: __dirname,
+		project: [tsConfigPath]
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		},
+		'import/resolver': {
+			node: {
+				paths: ['.']
+			}
+		}
+	},
+	env: {
+		browser: true,
+		node: true,
+		jest: true
+	},
+	plugins: [
+		'@typescript-eslint',
+		'react',
+		'react-hooks',
+		'prettier',
+		'import',
+		'jest-dom',
+		'testing-library',
+		'testcafe',
+		'sonarjs'
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:prettier/recommended',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+		'plugin:jest-dom/recommended',
+		'plugin:testing-library/react',
+		'plugin:testcafe/recommended',
+		'plugin:storybook/recommended',
+		'plugin:sonarjs/recommended'
+	],
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/unbound-method': 'off',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+		'import/order': [
+			'error',
+			{
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+					'type',
+					'object',
+					'unknown'
+				],
+				'newlines-between': 'never',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: false
+				}
+			}
+		]
+	}
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import App from './App'
+import { store } from './app/store'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>,
+	document.getElementById('root')
+)
