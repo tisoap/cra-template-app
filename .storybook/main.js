b86e700230e954ca21b20e3e14fb0a152a56cd3a
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-02-06 19:44:14 -0300

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/preset-create-react-app'
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: 'webpack5'
+	},
+	typescript: {
+		check: true
+	},
+	webpackFinal: async (config) => {
+		// Enable absolute path imports
+		config.resolve.modules.push(path.resolve(__dirname, '../'))
+		return config
+	}
+}
