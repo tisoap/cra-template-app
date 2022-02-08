2276daf08184299d30dad8432d37af15389803a5
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-02-08 10:36:06 -0300

    Add redux persist

--- src/providers/Store/index.tsx
+++ src/providers/Store/index.tsx
@@ -1,19 +1,27 @@
 import { Provider } from 'react-redux'
-import { store, rootReducer } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistedStore, rootReducer, persistor } from './store'
+import type { store } from './store'
 import type { ThunkAction, Action } from '@reduxjs/toolkit'
 import type { FunctionComponent } from 'react'
 
 export const StoreProvider: FunctionComponent = ({ children }) => {
-	return <Provider store={store}>{children}</Provider>
+	return (
+		<Provider store={persistedStore}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	)
 }
-
-export { store }
 
 export type AppDispatch = typeof store.dispatch
 
 export type AppState = ReturnType<typeof store.getState>
 
-export const initialState: AppState = rootReducer(undefined, { type: '@@INIT' })
+export const initialState: AppState = rootReducer(undefined, {
+	type: '@@INIT'
+})
 
 export type AppThunk<ReturnType = void> = ThunkAction<
 	ReturnType,
--- src/providers/Store/store.ts
+++ src/providers/Store/store.ts
@@ -1,4 +1,6 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { createStore, combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { counterReducer } from 'src/ducks/Counter'
 
 export const reducers = {
@@ -7,6 +9,16 @@ export const reducers = {
 
 export const rootReducer = combineReducers(reducers)
 
+export const persistConfig = {
+	key: 'root',
+	storage
+}
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
 	reducer: rootReducer
 })
+
+export const persistedStore = createStore(persistedReducer)
+export const persistor = persistStore(persistedStore)
